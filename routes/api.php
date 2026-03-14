09fb5cc6861d5194ecba470f5ee7273220e26d7c
Krisna Firdaus <krisnafirdaus@gmail.com>
2026-03-14 10:09:27 +0700

    feat: routing api

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Iluminate\Http\Request;
+use App\Http\Controllers\ProductController;
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('/products', [ProductController::class, 'store']);
