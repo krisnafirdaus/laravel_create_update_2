3e79fedfb408894c71a236314de146416ffefe2c
Krisna Firdaus <krisnafirdaus@gmail.com>
2026-03-14 11:32:09 +0700

    feat: get all

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,6 +8,34 @@ use Illuminate\Http\JsonResponse;
 
 class ProductController extends Controller
 {
+    public function index(Request $request): JsonResponse
+    {
+        // $products = Product::all();
+        $query = Product::query();
+
+        if($request->has('min_price')) {
+            $query->where('price', '>', $request->min_price);
+        }
+
+        if($request->has('in_stock')) {
+            $query->where('stock', '>', $request->in_stock);
+        }
+
+        $products = $query->paginate(10);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Products retrieved successfully',
+            'data' => $products->items(),
+            'meta' => [
+                'current_page' => $products->currentPage(),
+                'last_page' => $products->lastPage(),
+                'per_page' => $products->perPage(),
+                'total' => $products->total(),
+            ]
+        ]);
+    }
+
     public function store(Request $request): JsonResponse
     {
         // 1. validasi input
@@ -16,6 +44,9 @@ class ProductController extends Controller
             'description' => 'nullable|string|max:1000',
             'price' => 'required|integer|min:0',
             'stock' => 'required|integer|min:0',
+        ], [
+            'name.required' => 'Tolong dong nama produknya diisi',
+            'name.min' => 'Tolong dong minimal 3 karakter untuk nama produk',
         ]);
         
         // 2. simpan data ke database
