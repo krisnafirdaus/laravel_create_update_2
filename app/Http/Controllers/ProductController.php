b7e1e58c34860ee41bb8b642b45f2b255e3c02c2
Krisna Firdaus <krisnafirdaus@gmail.com>
2026-03-15 14:25:32 +0700

    feat: put api

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -57,7 +57,27 @@ class ProductController extends Controller
         return $this->successResponse($product, 'Produk berhasil dibuat', 201);
     }
 
-    public function update(Request $request, Product $product): JsonResponse
+    // public function update(Request $request, Product $product): JsonResponse
+    // {
+    //     // 1. validasi input
+    //     $validated = $request->validate([
+    //         'name' => 'sometimes|string|min:3|max:255',
+    //         'description' => 'nullable|string|max:1000',
+    //         'price' => 'sometimes|integer|min:0',
+    //         'stock' => 'sometimes|integer|min:0',
+    //     ], [
+    //         'name.required' => 'Tolong dong nama produknya diisi',
+    //         'name.min' => 'Tolong dong minimal 3 karakter untuk nama produk',
+    //     ]);
+        
+    //     // 2. update data
+    //     $product->update($validated);
+        
+    //     // 3. return response JSON
+    //     return $this->successResponse($product, 'Produk berhasil diupdate');
+    // }
+
+    public function update(Request $request, int $id): JsonResponse
     {
         // 1. validasi input
         $validated = $request->validate([
@@ -70,7 +90,8 @@ class ProductController extends Controller
             'name.min' => 'Tolong dong minimal 3 karakter untuk nama produk',
         ]);
         
-        // 2. update data
+        // 2. cari & update data
+        $product = Product::findOrFail($id);
         $product->update($validated);
         
         // 3. return response JSON
--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,10 @@ use App\Http\Controllers\ProductController;
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::post('/products', [ProductController::class, 'store']);
-Route::put('/products/{product}', [ProductController::class, 'update']);
-Route::patch('/products/{product}', [ProductController::class, 'update']);
+// Route::put('/products/{product}', [ProductController::class, 'update']);
+// Route::patch('/products/{product}', [ProductController::class, 'update']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::patch('/products/{id}', [ProductController::class, 'update']);
 
 // ctageory
 // user
